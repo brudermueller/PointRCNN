ea89d38a14f18448a1f1fdf31bdd98a303d80818
Lara Brudermüller <lara.brudermueller@tum.de>
2020-07-21 14:54:24 +0200

    WIP: adapt custom dataset class and utils

--- lib/datasets/custom_dataset.py
+++ lib/datasets/custom_dataset.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 '''
-    Pipeline for PointNet++ 
+    Data preparation for PointRCNN 
 '''
 
 import os 
@@ -10,11 +10,16 @@ import torch
 from torch.utils.data import Dataset
 
 import lib.utils.custom_data_utils as data_utils
+import lib.utils.roipool3d.roipool3d_utils as roipool3d_utils
+from lib.config import cfg
 
 # DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/med/hdf5/")
 
-class PointNetData(Dataset): 
-    def __init__(self, root, split, num_points, batch_size=10, normalize=False, intensity_channel=True, shuffle=True): 
+class CustomRCNNDataset(Dataset): 
+    def __init__(self, root, split='train', mode='TRAIN', num_points, batch_size=10, normalize=False, 
+                intensity_channel=True, shuffle=True, rcnn_training_roi_dir=None, 
+                rcnn_training_feature_dir=None, rcnn_eval_roi_dir=None,
+                rcnn_eval_feature_dir=None): 
         """
         :param root: directory path to the dataset
         :param split: 'train' or 'test'
@@ -25,11 +30,16 @@ class PointNetData(Dataset):
         """
         self.root = root
         self.split = split 
-        self.num_points = num_points 
+        self.num_points = num_points # TODO: define number of points to train with per frame
         self.batch_size = batch_size
         self.normalize = normalize
         self.intensity_channel = intensity_channel 
         self.shuffle = shuffle
+        self.classes = ('Background', 'Pedestrian')
+        self.num_class = self.classes.__len__()
+
+        self.sample_id_list = []
+        
 
         # load data files 
         all_files = data_utils.get_data_files(self.root)
@@ -39,7 +49,9 @@ class PointNetData(Dataset):
 
         data_batchlist, label_batchlist = [], []                                                                                       
         for f in all_files:                                                                                                            
-            data, labels = data_utils.load_h5(f)    
+            data, labels, bbox = data_utils.load_h5(f, bbox=True)    
+            #bbox: (N, 7) [x, y, z, h, w, l, ry]
+            centroid, h, w, l, angle = bbox[0:3], bbox[3], bbox[4], bbox[5], bbox[6] 
             if not intensity_channel: 
                 data = data[:,:3]                                                               
         
@@ -49,13 +61,37 @@ class PointNetData(Dataset):
             labels_reshaped[:, 0] = labels[:]
             labels = labels_reshaped
         
-            data_batchlist.append(data)                                      qoloLA                                                          
+            data_batchlist.append(data)                                                          
             label_batchlist.append(labels)    
         data_batches = np.concatenate(data_batchlist, 0)
         label_batches = np.concatenate(label_batchlist,0)
         
         self.data = data_batchlist
         self.labels = label_batchlist
+
+
+        # for rcnn training
+        self.rcnn_training_bbox_list = []
+        self.rpn_feature_list = {}
+        self.pos_bbox_list = []
+        self.neg_bbox_list = []
+        self.far_neg_bbox_list = []
+        self.rcnn_eval_roi_dir = rcnn_eval_roi_dir
+        self.rcnn_eval_feature_dir = rcnn_eval_feature_dir
+        self.rcnn_training_roi_dir = rcnn_training_roi_dir
+        self.rcnn_training_feature_dir = rcnn_training_feature_dir
+
+        assert mode in ['TRAIN', 'EVAL', 'TEST'], 'Invalid mode: %s' % mode
+        self.mode = mode
+
+
+    def get_lidar(self, idx):
+        #TODO
+        raise NotImplementedError
+
+    def get_label(self, idx):
+        #TODO
+        raise NotImplementedError    
 
     def __len__(self):
         return len(self.data)
@@ -65,13 +101,11 @@ class PointNetData(Dataset):
         pt_idxs = np.arange(0, self.num_points)
         np.random.shuffle(pt_idxs)
 
-        current_points = torch.from_numpy(self.points[idx, pt_idxs].copy()).float()
+        current_points = torch.from_numpy(self.data[idx, pt_idxs].copy()).float()
         current_labels = torch.from_numpy(self.labels[idx, pt_idxs].copy()).long()
 
         return current_points, current_labels
-        
-        
-        return self.data, self.labels
+        # return self.data, self.labels
 
 
 
---
+++ lib/utils/custom_object3d.py
@@ -0,0 +1,81 @@
+
+class Object3d(object):
+    def __init__(self, bbox_array): 
+        #bbox: (array of length 7) [x, y, z, h, w, l, ry]
+        self.centroid = bbox[0:3]
+        self.h, self.w, self.l = float(bbox[3]), float(bbox[4]), float(bbox[5])
+        self.ry =  bbox[6]  # orientation angle 
+        # According to KITTI definition
+        self.cls_type = 'Pedestrian'
+        self.cls_id = 2
+
+    def generate_corners3d(self):
+        """
+        Generate corners3d representation for this object
+        :return corners_3d: (8, 3) corners of box3d in camera coord
+        """
+        l, h, w = self.l, self.h, self.w
+        # careful: width, length and height have been differently defined than in KITTI
+        y_corners = [l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2]
+        z_corners = [0, 0, 0, 0, -h, -h, -h, -h]
+        x_corners = [w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2]
+
+        R = np.array([[np.cos(self.ry), 0, np.sin(self.ry)],
+                      [0, 1, 0],
+                      [-np.sin(self.ry), 0, np.cos(self.ry)]])
+        corners3d = np.vstack([x_corners, y_corners, z_corners])  # (3, 8)
+        corners3d = np.dot(R, corners3d).T
+        corners3d = corners3d + self.pos
+        return corners3d
+
+    def calc_3d_box(self): 
+        """ 
+        Generate 3d bounding box with 8 vertices saved as np.array (of shape (8,3)) for the 
+        3d box in following order:
+            7 -------- 6
+           /|         /|
+          4 -------- 5 .
+          | |        | |
+          . 3 -------- 2
+          |/         |/
+          0 -------- 1
+
+        Returns:
+            bbox np.ndarray
+        """
+        x, y, z, h, w, l = self.x, self.y, self.z, self.h, self.w, self.l
+        box8 = np.array(
+            [
+                [
+                    x + w / 2,
+                    x + w / 2,
+                    x - w / 2,
+                    x - w / 2,
+                    x + w / 2,
+                    x + w / 2,
+                    x - w / 2,
+                    x - w / 2,
+                ],
+                [
+                    y - l / 2,
+                    y + l / 2,
+                    y + l / 2,
+                    y - l / 2,
+                    y - l / 2,
+                    y + l / 2,
+                    y + l / 2,
+                    y - l / 2,
+                ],
+                [
+                    z - h / 2,
+                    z - h / 2,
+                    z - h / 2,
+                    z - h / 2,
+                    z + h / 2,
+                    z + h / 2,
+                    z + h / 2,
+                    z + h / 2,
+                ],
+            ]
+        )
+        return box8.T

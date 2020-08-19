53431b33d611c2e36d02ee89c941fc91e07d5273
Lara Brudermüller <lara.brudermueller@tum.de>
2020-08-19 10:21:00 +0200

    Change bbox creation to be in velodyne coords

--- lib/utils/custom_object3d.py
+++ lib/utils/custom_object3d.py
@@ -3,10 +3,11 @@ import numpy as np
 
 class CustomObject3d(object):
     def __init__(self, bbox_array): 
-        #bbox: (array of length 7) [x, y, z, h, w, l, ry]
-        self.centroid = bbox[0:3]
+        #bbox: (array of length 7) [x, y, z, h, w, l, rz]
+        self.pos = bbox[0:3]
         self.h, self.w, self.l = float(bbox[3]), float(bbox[4]), float(bbox[5])
-        self.ry =  bbox[6]  # orientation angle 
+        self.rz =  bbox[6]  # rotation angle around z-axis (instead of y as in camera coord.)
+        self.dis_to_sensor = np.linalg.norm(self.pos)
         # According to KITTI definition
         self.cls_type = 'Pedestrian'
         self.cls_id = 2
@@ -14,23 +15,32 @@ class CustomObject3d(object):
     def generate_corners3d(self):
         """
         Generate corners3d representation for this object
-        :return corners_3d: (8, 3) corners of box3d in camera coord
+            7 -------- 6
+           /|         /|
+          4 -------- 5 .
+          | |        | |
+          . 3 -------- 2
+          |/         |/
+          0 -------- 1
+        :return corners_3d: (8, 3) corners of oriented box3d in Velodyne coord.
         """
         l, h, w = self.l, self.h, self.w
         # careful: width, length and height have been differently defined than in KITTI
-        y_corners = [l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2, -l / 2]
-        z_corners = [0, 0, 0, 0, -h, -h, -h, -h]
-        x_corners = [w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2, w / 2]
+        x_corners = [w / 2, w / 2, -w / 2, -w / 2, w / 2, w / 2, -w / 2, -w / 2]        
+        y_corners = [-l / 2, l / 2, l / 2, -l / 2, -l / 2, l / 2, l / 2, -l / 2]
+        z_corners = [0, 0, 0, h / 2, h / 2, h / 2]
 
-        R = np.array([[np.cos(self.ry), 0, np.sin(self.ry)],
-                      [0, 1, 0],
-                      [-np.sin(self.ry), 0, np.cos(self.ry)]])
+        # rotation now defined in Velodyne coords. -> around z-axis => yaw rot. 
+        R = np.array([[np.cos(self.rz), -np,sin(self.rz), 0],
+                      [np.sin(self.rz), np.cos(self.rz), 0],
+                      [0, 0, 1]])
         corners3d = np.vstack([x_corners, y_corners, z_corners])  # (3, 8)
+        # transpose and rotate around orientation angle 
         corners3d = np.dot(R, corners3d).T
         corners3d = corners3d + self.pos
         return corners3d
 
-    def calc_3d_box(self): 
+    def generate_unoriented_bbox(self): 
         """ 
         Generate 3d bounding box with 8 vertices saved as np.array (of shape (8,3)) for the 
         3d box in following order:
